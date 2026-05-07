70d9429ceac6b5b9922e38080b71a7442bf0fc27
Eric Orr <eric.orr.dev@gmail.com>
2026-05-07 15:02:21 -0700

    changed hero text

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export default function HeroSection() {
 	return (
 		<Card className="bg-industrial-black/98 rounded-3xl shadow-lg p-10 text-center backdrop-blur-md">
-			<h1 className="text-5xl md:text-6xl font-bold mb-6 leading-tight text-white">
-				Engineering for the Physical World.
+			<h1 className="text-5xl md:text-5xl font-bold mb-6 leading-tight text-white">
+				Friends, Romans, countrymen, lend me your ears
 			</h1>
 			<p className="text-xl text-white mb-8 max-w-2xl mx-auto">
 				I&apos;m a CS student, and this is my blog about software engineering,
